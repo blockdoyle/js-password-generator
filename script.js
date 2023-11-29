04e5b6c99d452a7b5b182974adbc976cf20d40f5
Brock Lockhart-Doyle <blockdoyle@gmail.com>
2023-11-28 23:38:55 -0500

    added userChoice object

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  // Get user input for password generation options
+  var userChoice = {
+    length: window.prompt("Enter password length"),
+    useUpperCase: window.prompt("Use uppercase letters? Y/N"),
+    useLowerCase: window.prompt("Use lowercase letters? Y/N"),
+    useSpecialChars: window.prompt("Use special characters? Y/N")
+  }
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
